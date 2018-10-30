bfc2b8790e821413ddfbc49c0604ff35fb623b1f
sergeytsarev90 <sergeytsarev90@gmail.com>
2018-10-30 12:28:02 +0300

    Осуществил запуск 3-х браузеров

--- lesson_3_4.py
+++ lesson_3_4.py
@@ -1,29 +1,40 @@
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 @pytest.fixture
+
 def driver_firefox(request):
     wd = webdriver.Firefox()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
+@pytest.fixture
 def driver_chrome(request):
     wd = webdriver.Chrome()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
+@pytest.fixture
+def driver_ie(request):
+    wd = webdriver.Ie(capabilities={"requireWindowFocus": True})
+    request.addfinalizer(wd.quit)
+    return wd
 
-def test_example(driver):
-    driver_firefox.get("http://localhost/litecart/admin/login.php")
-    driver_firefox.find_element_by_name("username").send_keys("admin")
-    driver_firefox.find_element_by_name("password").send_keys("admin")
-    driver_firefox.find_element_by_name("login").click()
-    WebDriverWait(driver_firefox, 10).until(EC.title_is("My Store"))
+def test_example_chrome(driver_chrome):
+    driver_chrome.get("http://www.google.com")
+    WebDriverWait(driver_chrome, 10).until(EC.title_is("Google"))
 
+def test_example_firefox(driver_firefox):
+    driver_firefox.get("http://www.google.com")
+    WebDriverWait(driver_firefox, 10).until(EC.title_is("Google"))
+
+def test_example_ie(driver_ie):
+    driver_ie.get("http://www.google.com")
+    WebDriverWait(driver_ie, 10).until(EC.title_is("Google"))
 
 
