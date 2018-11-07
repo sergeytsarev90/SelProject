e9544fe826086364b4eb883d686d27a2dc6a15cd
sergeytsarev90 <sergeytsarev90@gmail.com>
2018-11-07 17:16:21 +0300

    1

--- lesson_2_3.py
+++ lesson_2_3.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome()
+    wd = webdriver.Firefox()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
