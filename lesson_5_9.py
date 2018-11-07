2c0173fafc37007ae15757cf85770bd8dc75aaad
sergeytsarev90 <44232706+sergeytsarev90@users.noreply.github.com>
2018-11-07 23:15:37 +0300

    Урок 5 задание 9 не могу понять как нажимать города с зонами

--- lesson_5_9.py
+++ lesson_5_9.py
@@ -14,13 +14,6 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
-def is_element_present(element, args):
-  try:
-    element.find_element_by_css_selector(args)
-    return True
-  except NoSuchElementException:
-    return False
-
 
 def test_example(driver):
     driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
@@ -29,10 +22,33 @@ def test_example(driver):
     driver.find_element_by_name("login").click()
     #WebDriverWait(driver, 10).until(EC.title_is("My Store"))
 
-    elements = driver.find_elements_by_css_selector('[name=countries_form]')
+    #elements = driver.find_elements_by_css_selector('[name=countries_form]')
+    elements = driver.find_elements_by_xpath("//*[contains(@class,'row')]")
+
+    countries = []
+
+    zone_countries = []
+
+
     for element in elements:
-        temp = element.find_element_by_css_selector('[href]').text
-        print(temp)
+        countries.append(element.find_element_by_css_selector('[href]').text)
+        if(int(element.find_element_by_css_selector("td:nth-child(6)").text) > 0):
+            zone_countries = element.find_element_by_css_selector("td:nth-child(4)")
+
+
+    countries_1 = sorted(countries)
+    print(zone_countries)
+    a=0
+
+
+    element.find_element_by_css_selector('td:nth-child(4)')
+
+    while a < countries.__len__():
+        if(countries[a] != countries_1[a]):
+            print(countries[a], ' страна расположена не по алфавиту')
+        a+=1
+
+
 
 
 
